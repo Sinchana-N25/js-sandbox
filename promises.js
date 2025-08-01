b21e00c5e37bf149d95128ecaf77f1d08ca9a7da
Sinchana <sinchana2505@gmail.com>
2025-08-01 10:57:25 +0530

    Day 4

---
+++ promises.js
@@ -0,0 +1,35 @@
+//Day 4
+// Focus: Async/Await and Promises
+// Tasks Completed:
+// - Created a basic promise and consumed it with .then() and .catch()
+// - Used async/await to fetch data from an external API (dummyjson.com)
+// - Handled fetch errors with try-catch and simulated loading delay
+
+function getSomeData() {
+  let error = false;
+
+  return new Promise((resolve, reject) => {
+    if (!error) {
+      setTimeout(() => resolve("Data Fetched!"), 2000);
+    } else {
+      reject("An error happened along the way.");
+    }
+  });
+}
+
+getSomeData()
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+async function getUser() {
+  let response = await fetch(
+    "https://dummyjson.com/users?limit=5&skip=10&select=firstName,age"
+  );
+  if (response.ok === false) {
+    throw new Error("Something went wrong. 😦");
+  }
+  let data = await response.json();
+  setTimeout(() => console.log(data), 1000);
+}
+
+getUser();

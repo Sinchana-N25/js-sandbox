b37ea7b7b8dfc3ddded9b64e6c29b01002a14fe7
Sinchana N <sinchana2505@gmail.com>
2025-08-11 10:42:51 +0530

    learnt UseEffect hook

---
+++ colours.js
@@ -0,0 +1,26 @@
+﻿//Math decides the colour of the background, a simple example for UseEffect hook
+import React from "react";
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [colour, setColour] = useState("#ffffff");
+
+  useEffect(
+    function () {
+      document.body.style.backgroundColor = colour;
+    },
+    [colour]
+  );
+
+  function generateColour() {
+    const randomColour =
+      "#" + Math.floor(Math.random() * 16777215).toString(16);
+    setColour(randomColour);
+  }
+
+  return (
+    <div id="button">
+      <button onClick={generateColour}>ZAP!!</button>
+    </div>
+  );
+}

ef1c2f4f3e52f76768e10c40f6e672977a14dd17
Sinchana N <sinchana2505@gmail.com>
2025-08-11 20:12:41 +0530

    learnt node basics

---
+++ node_practice/favourite_lyric.js
@@ -0,0 +1,9 @@
+﻿//run in terminal with node
+//go to the node_practice folder by using cd in terminal(powershell)
+// and then type: node favourite_lyric.js
+
+//Node.js is a JavaScript runtime environment that lets you
+// run your own server and interact with your operating system
+console.log("You used to be alright / What happened? / ");
+console.log("Did the cat get your tongue / Did your string come undone ");
+console.log("- 15 Step by Radiohead");
